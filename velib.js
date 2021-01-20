e1451f8c79b363ab5f021913a5b540f8d2728e78
Bertrand <dan.bertrand.hs@gmail.com>
2021-01-20 17:18:11 +0100

    Add localisation

--- velib.js
+++ velib.js
@@ -1,23 +1,18 @@
-const URL = "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&rows=100&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes"
+class VelibStation {
 
-let object;
-let array = [];
+    constructor(name, capacity, mechanicalBike, eBike, docksAvalaible, geoLoc) {
+        this.name = name;
+        this.capacity = capacity;
+        this.mechanicalBike = mechanicalBike;
+        this.eBike = eBike
+        this.docksAvalaible = docksAvalaible
+        this.geoLoc = geoLoc;
+    };
 
-class VelibStation{
-	constructor(name, capacity, mechanicalBike, eBike, docksAvalaible, geoLoc){
-		this.name           = name;
-		this.capacity       = capacity;
-		this.mechanicalBike = mechanicalBike;
-		this.eBike          = eBike
-		this.docksAvalaible = docksAvalaible
-		this.geoLoc         = geoLoc;
-	};
+    show(id) {
 
-
-	show(){
-
-		let section       = document.createElement("DIV");
-		section.innerHTML = `
+        let section = document.createElement("DIV");
+        section.innerHTML = `
 			<div class="card" style="width: 20rem;">
 				<div class="card-body">
 					<h2 class="card-title">${this.name} </h2>
@@ -27,52 +22,68 @@ class VelibStation{
 						<li>Docks avalaible : ${this.docksAvalaible}</li>
 						<li>Total capacity : ${this.capacity}</li>
 					</ul>
+					<div class="map" id="map${id}"></div>
 				</div>
 			</div>
 		`
-		document.getElementById("display").appendChild(section);
-	}
+        document.getElementById("display").appendChild(section);
+
+        this.map(id)
+    };
+
+    map(id) {
+        let mymap = L.map(`map${id}`).setView([`${this.geoLoc[0]}`, `${this.geoLoc[1]}`], 13);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: "pk.eyJ1IjoiZGFuNzQiLCJhIjoiY2trNWxrZXA0MGRieDJ2bHd0cGl5YXA5eCJ9.0z-bOKn4CtcHEcgjpGc44A"
+        }).addTo(mymap);
+
+        let marker = L.marker([`${this.geoLoc[0]}`, `${this.geoLoc[1]}`]).addTo(mymap);
+    };
 };
 
 updateInfo = () => {
-	let object;
-	let array = [];
 
-	fetch(URL)
-	  .then((response) => response.json() )
-	  .then((item) =>  {
+    const URL = "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&rows=100&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes"
+    let object;
+    let array = [];
 
-	  		for( i = 0; i < 100; i++ ){
-	  			  		object = item.records[i].fields
+    fetch(URL)
+        .then((response) => response.json())
+        .then((item) => {
 
-	  					let name           = object.name;
-	  					let mechanicalBike = object.mechanical;
-	  					let geoLoc 		   = object.coordonnees_geo;
-	  					let eBike          = object.ebike;
-	  					let docksAvalaible = object.numdocksavailable;
-	  					let capacity       = object.capacity;
+            for (i = 0; i < 100; i++) {
+                object = item.records[i].fields
 
-	  					array.push(new VelibStation(name, capacity, mechanicalBike, eBike, docksAvalaible, geoLoc));
-	  		}
+                let name = object.name;
+                let mechanicalBike = object.mechanical;
+                let geoLoc = object.coordonnees_geo;
+                let eBike = object.ebike;
+                let docksAvalaible = object.numdocksavailable;
+                let capacity = object.capacity;
 
-	  		let mainDiv = document.querySelector("#display");
-	  		
-	  		mainDiv.innerHTML = "";
+                array.push(new VelibStation(name, capacity, mechanicalBike, eBike, docksAvalaible, geoLoc));
+            }
 
+            let mainDiv = document.querySelector("#display");
+            mainDiv.innerHTML = "";
 
+            let id = 0;
+            array.forEach((station) => {
+                station.show(id)
+                id += 1
+            });
 
-	  		array.forEach((station) => {
-	  			station.show()
-	  		});
-	  		
-	  });
+        });
 };
 
 start = () => {
-  updateInfo();
-  setInterval(function(){ updateInfo()}, 60000);
+    updateInfo();
+    setInterval(function() { updateInfo() }, 60000);
 };
 
-
 start();
-
